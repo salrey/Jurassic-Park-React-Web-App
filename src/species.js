adb4a76ce2cb068c010b12d4b31576c5d5e5a864
salrey <tekdehn@gmail.com>
2021-11-10 10:14:55 -0500

    Add App css and species

---
+++ src/species.js
@@ -0,0 +1,32 @@
+const species = [
+    {
+      name: "Deinonychus",
+      image: "https://images.dinosaurpictures.org/deinonychus-1_3300.jpg",
+    },
+    {
+      name: "Tyrannosaurus Rex",
+      image:
+        "https://static.turbosquid.com/Preview/2015/06/20__18_36_28/Trex1.jpg4a5e294e-b0e7-4c87-b112-4c5438def66bDefaultHQ.jpg",
+    },
+    {
+      name: "Brachiosaurus",
+      image:
+        "https://i.pinimg.com/originals/94/12/1d/94121da556005c19b3596999d8a44640.jpg",
+    },
+    {
+      name: "Ankylosaur",
+      image: "http://www.abc.net.au/news/image/7009930-3x2-700x467.jpg",
+    },
+    {
+      name: "Triceratops",
+      image:
+        "https://www.scottishconstructionnow.com/wp-content/uploads/sites/11/2017/09/Triceratops.jpg",
+    },
+    {
+      name: "Spinosaur",
+      image:
+        "https://utahhome.me/wp-content/uploads/2017/01/GettyImages-157684735.jpg",
+    },
+  ];
+  ​
+  export default species;
