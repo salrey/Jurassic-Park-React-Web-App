ea04f0605895a2cb383531bba394bf12c620f0e0
salrey <tekdehn@gmail.com>
2021-11-10 11:59:46 -0500

    Set state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import species from './data/species';
 import Headings from './components/Headings';
 
 
-function App() {
-  return (
-    <div className="App">
-     <Headings />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      dinos: [],
+      numberOfDinos: 0,
+      currentSpecies: species[0],
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <Headings />
+      </div>
+    );
+  }
 }
 
 export default App;
